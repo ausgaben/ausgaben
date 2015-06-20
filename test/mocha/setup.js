b0904c45fd02b161c83be746193a9ce3c21ec356
Markus Tacker <m@coderbyheart.de>
2015-06-20 21:01:04 +0200

    Clean DB before test.

---
+++ test/mocha/setup.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var sequelize = require('../../server/config/sequelize');
+var request = require('supertest');
+var bluebird = require('bluebird');
+bluebird.promisifyAll(request);
+
+before(function () {
+    // recreating the sequelize takes a bit => disable timeout
+    this.timeout(0);
+    console.log('Recreate all tables');
+    // Synchronizing any model changes with database.
+    return sequelize.sync({
+        force: true
+    }).catch(function (err) {
+        console.error(err);
+        process.exit(1);
+    });
+});
